840e11449f97070970c71e29ee150e89b77b4a57
Stephen Bolt on NB-99 <stephen.m.bolt@gmail.com>
2018-02-12 20:32:55 -0600

    Adding some code to initGs.

--- src/gs.js
+++ src/gs.js
@@ -1,7 +1,14 @@
+
+
 function initGs(args) {
     // this method initializes module gs
     this.args = args;
     console.log('Module gs is initialized.');
+    var gs = {
+        prop1: 'value1',
+        prop2: 'value2',
+        method1: () => { return 'this is from method1'; }
+    };
 }
 
 module.exports = initGs;
